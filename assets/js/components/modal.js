164a029bc32b2c0278669f0b46102d89a40171a5
manu-cj <carenjotje@gmail.com>
2024-05-21 11:15:23 +0200

    save

---
+++ assets/js/components/modal.js
@@ -0,0 +1,11 @@
+function addEventModal() {
+    const modalSection = document.createElement('section');
+    const form = document.createElement('form');
+
+    
+}
+
+
+function deleteEventModal() {
+    
+}
