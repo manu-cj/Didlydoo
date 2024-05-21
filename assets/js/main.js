9a649cd616d6fdb621629307c49c814d646eb219
Arnaud Van Acker <arnaudvanacker@yahoo.fr>
2024-05-21 15:57:11 +0200

    add intro.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
+import { intro } from "./components/intro.js";
 import eventListenner from "./components/eventlistenner.js";
 import { switchDarkmode } from "./components/darkmode.js";
 import { footerLinks } from "./components/footerLinks.js";
+
+intro()
 
 eventListenner();
 
