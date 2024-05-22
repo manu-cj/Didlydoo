c67c2187659884a42e6085422c47142c35c5f998
Arnaud Van Acker <arnaudvanacker@yahoo.fr>
2024-05-22 11:45:32 +0200

    dancing cats

---
+++ assets/js/components/cats.js
@@ -0,0 +1,23 @@
+import { sleep } from "./functions.js";
+
+export async function cats() {
+    let randomCat,animation;
+    const cats = Array.from(document.querySelectorAll('.cats'))
+
+    while(true) {
+        await sleep(getRandomInt(3000))
+
+        randomCat = cats[getRandomInt(3)-1]
+        animation = 'cat'+getRandomInt(3)
+
+        randomCat.classList.add(animation)
+        await sleep(500)
+        randomCat.classList.remove(animation)
+
+        await sleep(100)
+    }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max)+1;
+  }
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,14 @@ import { intro } from "./components/intro.js";
 import eventListenner from "./components/eventlistenner.js";
 import { switchDarkmode } from "./components/darkmode.js";
 import { footerLinks } from "./components/footerLinks.js";
-
+import { cats } from "./components/cats.js";
 
 intro()
 
-eventListenner();
+eventListenner()
 
 switchDarkmode()
 
 footerLinks()
+
+cats()
