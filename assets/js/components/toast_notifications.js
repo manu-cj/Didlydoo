9701440c3a7198062758da4d819659f7c85e8ec2
manu-cj <carenjotje@gmail.com>
2024-05-27 13:40:24 +0200

    add style and responsive

--- assets/js/components/toast_notifications.js
+++ assets/js/components/toast_notifications.js
@@ -55,7 +55,7 @@ function notif(etat, text, parent) {
         } else {
             console.log('Toast key is not present or has a different value.');
         }
-    },4000)
+    },2000)
     setTimeout(() => {
         localStorage.setItem('toast', "null");
         parent.style.animationName = "delete-toast";
